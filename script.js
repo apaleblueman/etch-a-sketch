c863c609667cc8a9ea4ab370fa877fd70424285e
apaleblueman <gurchetan.singh2@icloud.com>
2024-04-15 19:36:35 +0530

    Add working sketchPad

--- script.js
+++ script.js
@@ -1,23 +1,58 @@
-//creating grids
-
-let sketchpad = document.querySelector("#sketchPad");
-
-document.addEventListener('DOMContentLoaded', (event)=>{
-    let cells = 12;
-    let h = (500/cells)-1.8;
-    let w =(500/cells)-1.8;
-    for(let row=0;row<cells*cells;row++){
-            let cell = document.createElement("div");
-            cell.style.border="solid 1px"        
-            cell.style.width=`${w}px`
-            cell.style.height=`${h}px`
-            sketchpad.appendChild(cell);
+//start with 16X16 grid
+makeGrid(16,Math.floor((500/16)));
+let makeGridButton = document.querySelector("#buttons");
+makeGridButton.addEventListener('click', (event)=>{
+    let cellNum = parseInt(prompt("Enter dimentions"));
+    if(cellNum>100 || cellNum < 1){
+     alert("Enter a valid number!");
+    }else{
+        let cellSize = Math.floor((500/cellNum));
+        makeGrid(cellNum,cellSize);
     }
 })
 
+//random colors
+function getRainbow() {
+    let colorCode = Math.floor(Math.random() * 7);
+    switch(colorCode) {
+        case 0:
+            return "red";
+        case 1:
+            return "orange";
+        case 2:
+            return "yellow";
+        case 3:
+            return "green";
+        case 4:
+            return "blue";
+        case 5:
+            return "indigo";
+        case 6:
+            return "violet";
+        default:
+            return "black"; // default to black if something unexpected happens
+    }
+}
 
-let changeSize = document.querySelector("#ui");
 
-changeSize.addEventListener('click' , (event)=>{
-    cells = parseInt(prompt("Enter size"))
-})
+//make a grid
+function makeGrid(numCells,cellSize){
+    let canvas = document.querySelector("#sketchPad");
+    canvas.innerHTML="";
+    for(let cellIter = 0; cellIter<numCells; cellIter++){
+        let rowDiv = document.createElement("div");
+        rowDiv.style.display="flex";
+        canvas.appendChild(rowDiv);
+        for(let cellIter = 0; cellIter<numCells; cellIter++){
+            let colDiv = document.createElement("div");
+            colDiv.style.width=`${cellSize}px`;
+            colDiv.style.height=`${cellSize}px`;
+            colDiv.style.border="solid 1px black";
+            rowDiv.appendChild(colDiv);
+            colDiv.addEventListener('mouseover',()=>{
+                colDiv.style.backgroundColor="black";
+            });
+            
+        }
+    }
+}
